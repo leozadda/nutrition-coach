8fd79c7e46d1748121ba655af1edd854a3fabc47
Leo <leo@Leos-MacBook-Pro.local>
2025-09-17 11:49:19 -0500

    removed b-lu-e logo

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import LOGO from './B-L-U-E.png';
+import { UtensilsCrossed } from 'lucide-react';
 
 export default function Footer() {
   return (
     <footer className="bg-OFFWHITE text-BLUE p-4 sm:p-6 font-serif">
       <div className="max-w-6xl mx-auto flex flex-col sm:flex-row justify-between items-center">
         <div className="flex items-center mb-4 sm:mb-0">
-          <img 
-            className="w-10 sm:w-12 mr-3" 
-            src={LOGO}
-            alt="Logo" 
-          />
-          <span className="text-sm sm:text-base">
-            © 2024 BLUE
+          <UtensilsCrossed />
+          <span className="text-sm sm:text-base p-2">
+            Forkgiveness © 2025
           </span>
         </div>
         <div className="flex space-x-4 sm:space-x-6">
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import LOGO from './B-L-U-E.png';
+import { UtensilsCrossed } from 'lucide-react';
 
 const CALENDLY_LINK = "https://calendly.com/leozadda/talking-about-health-and-fitness-goals";
 
 export default function Header() {
   return (
     <div className="bg-OFFWHITE min-h-screen flex flex-col justify-center items-center font-serif text-center px-4">
-      <img className="w-24 sm:w-28 md:w-32 lg:w-36 pb-6" src={LOGO} alt="Logo" />
+      <UtensilsCrossed size={64} color="#0000ff" strokeWidth={2.25} />
       <h1 className='text-BLUE text-6xl sm:text-7xl md:text-8xl lg:text-8xl xl:text-9xl mb-8 py-6 sm:py-8 md:py-10'>Your Fork is Your Weapon.</h1>
       <a href={CALENDLY_LINK}>
         <button className='px-6 sm:px-8 md:px-10 py-2 sm:py-3 bg-BLUE text-OFFWHITE rounded-full text-2xl sm:text-3xl md:text-4xl lg:text-5xl transition-colors font-sans hover:bg-OFFWHITE hover:text-BLUE hover:border border-BLUE'>Free Call</button>
